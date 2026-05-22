0cb16308b1c528525983f34c5c782d8588c42dfd
Satriart2 <mobilberjalan20@gmail.com>
2026-05-22 11:23:47 +0800

    Create Subscription endpoint, model, migration

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\SubscriptionController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::apiResource('subscriptions', SubscriptionController::class);
