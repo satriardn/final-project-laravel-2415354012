af85b05cb944dd8f0c22a959fcd76136980ca332
Satriart2 <mobilberjalan20@gmail.com>
2026-05-22 11:12:05 +0800

    Create customer end point, model, migration

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\CustomerController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('customers', CustomerController::class);
+Route::patch('customers/{customer}/activate', [CustomerController::class, 'activate']);
+Route::patch('customers/{customer}/deactivate', [CustomerController::class, 'deactivate']);
