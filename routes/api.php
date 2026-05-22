e6c319095f813b982e992635f7ad63b6810ea77c
Satriart2 <mobilberjalan20@gmail.com>
2026-05-22 10:57:34 +0800

    Create service migration

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ServiceController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::apiResource('services', ServiceController::class);
+Route::patch('services/{service}/activate', [ServiceController::class, 'activate']);
+Route::patch('services/{service}/deactivate', [ServiceController::class, 'deactivate']);
