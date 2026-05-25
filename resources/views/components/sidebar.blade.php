298ed15c6e1a28ad1f778941213c49c10cedee6a
Satriart2 <mobilberjalan20@gmail.com>
2026-05-25 14:11:04 +0800

    update UI

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -47,19 +47,3 @@
     </nav>
 </aside>
 
-<script>
-    document.addEventListener('DOMContentLoaded', () => {
-        const sidebar = document.getElementById('sidebar');
-        const toggle = document.getElementById('sidebar-toggle');
-
-        if (toggle) {
-            toggle.addEventListener('click', () => {
-                sidebar.classList.toggle('sidebar-collapsed');
-                const header = sidebar.querySelector('.sidebar-header');
-                header.classList.toggle('justify-between');
-                header.classList.toggle('justify-center');
-            });
-        }
-    });
-</script>
-
